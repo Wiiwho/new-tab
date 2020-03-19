56a85c6f50c6de22e8aa7b892df31d319671a0f8
Eliyahu <eliyahumizrahi123@gmail.com>
2020-03-19 00:13:08 -0400

    added shortcut cards

--- src/App.js
+++ src/App.js
@@ -19,6 +19,47 @@ const WelcomeText = styled.p`
   margin-top: 0px;
 `;
 
+const Shortcuts = styled.div`
+  display: flex;
+  max-width: 400px;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const ShortcutCard = styled.div`
+  height: 70px;
+  width: 70px;
+  background: white;
+  border-radius: 160px;
+  box-shadow: 1px 1px 40px -15px rgba(0, 0, 0, 0.65);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ShortcutText = styled.p`
+  font-family: Cairo;
+  font-size: 14px;
+  color: black;
+  margin-top: 5px;
+  margin-bottom: 0px;
+`;
+
+const ShortcutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 15px;
+  padding-right: 15px;
+  cursor: pointer;
+  :hover {
+    background: #aaa;
+  }
+`;
+
 const SearchCard = styled.div`
   position: relative;
   height: 50px;
@@ -29,6 +70,7 @@ const SearchCard = styled.div`
   display: flex;
   align-items: center;
   padding-left: 20px;
+  margin-bottom: 10px;
 `;
 
 const SearchField = styled.input`
@@ -55,6 +97,73 @@ function App() {
           placeholder="Search using Google..."
         ></SearchField>
       </SearchCard>
+      <Shortcuts>
+        <ShortcutContainer>
+          <ShortcutCard>
+            <img
+              src="https://icons.duckduckgo.com/ip3/twitter.com.ico"
+              alt="Favicon"
+            />
+          </ShortcutCard>
+          <ShortcutText>bruh2</ShortcutText>
+        </ShortcutContainer>
+        <ShortcutContainer>
+          <ShortcutCard></ShortcutCard>
+          <ShortcutText>bruh2</ShortcutText>
+        </ShortcutContainer>
+        <ShortcutContainer>
+          <ShortcutCard></ShortcutCard>
+          <ShortcutText>bruh2</ShortcutText>
+        </ShortcutContainer>
+        <ShortcutContainer>
+          <ShortcutCard></ShortcutCard>
+          <ShortcutText>bruh2</ShortcutText>
+        </ShortcutContainer>
+        <ShortcutContainer>
+          <ShortcutCard></ShortcutCard>
+          <ShortcutText>bruh2</ShortcutText>
+        </ShortcutContainer>
+        <ShortcutContainer>
+          <ShortcutCard></ShortcutCard>
+          <ShortcutText>bruh2</ShortcutText>
+        </ShortcutContainer>
+        <ShortcutContainer>
+          <ShortcutCard></ShortcutCard>
+          <ShortcutText>bruh2</ShortcutText>
+        </ShortcutContainer>
+        <ShortcutContainer>
+          <ShortcutCard></ShortcutCard>
+          <ShortcutText>bruh2</ShortcutText>
+        </ShortcutContainer>
+        <ShortcutContainer>
+          <ShortcutCard></ShortcutCard>
+          <ShortcutText>bruh2</ShortcutText>
+        </ShortcutContainer>
+        <ShortcutContainer>
+          <ShortcutCard></ShortcutCard>
+          <ShortcutText>bruh2</ShortcutText>
+        </ShortcutContainer>
+        <ShortcutContainer>
+          <ShortcutCard></ShortcutCard>
+          <ShortcutText>bruh2</ShortcutText>
+        </ShortcutContainer>
+        <ShortcutContainer>
+          <ShortcutCard></ShortcutCard>
+          <ShortcutText>bruh2</ShortcutText>
+        </ShortcutContainer>
+        <ShortcutContainer>
+          <ShortcutCard></ShortcutCard>
+          <ShortcutText>bruh2</ShortcutText>
+        </ShortcutContainer>
+        <ShortcutContainer>
+          <ShortcutCard></ShortcutCard>
+          <ShortcutText>bruh2</ShortcutText>
+        </ShortcutContainer>
+        <ShortcutContainer>
+          <ShortcutCard></ShortcutCard>
+          <ShortcutText>bruh2</ShortcutText>
+        </ShortcutContainer>
+      </Shortcuts>
     </Container>
   );
 }
